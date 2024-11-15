76b7512b66410749e7fbd18f5b0bd20765e3aae5
Kelvin Nyingi <adikelvin1@gmail.com>
2024-11-15 16:20:44 +0100

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
   root "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
